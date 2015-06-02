e7350d728b55fe9baca472f76bb79b36538b6b36
plasmashadow <plasmashadowx@gmail.com>
2015-06-02 12:09:44 +0530

    Adding new changes to setup.py

--- setup.py
+++ setup.py
@@ -6,13 +6,11 @@ import os
 def Readme():
     return open(os.path.join(os.path.dirname(__file__), 'README.md'), "r").read()
 
-
 setup(
     name='pyrequest',
     packages=['pyhttp'],
-    version='0.2',
+    version='0.3',
     description='Pythonic way of HTTP request',
-    long_description=Readme(),
     author='plasmashadow',
     author_email='plasmashadowx@gmail.com',
     url='https://github.com/plasmashadow/pyhttp.git',
